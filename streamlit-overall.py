53ce919a92582fba3e0c6d5eef4d96937cb32284
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-04 13:25:30 +0800

    Update streamlit-overall.py

--- streamlit-overall.py
+++ streamlit-overall.py
@@ -71,23 +71,27 @@ merged_dfs = []
 for filename in df_totals['filename']:
     # Loop through the subfolders
     for subfolder in subfolders:
+        st.write(subfolder)
         # Construct the URL for the subfolder
         subfolder_url = f"{url}/{subfolder}"
+        st.write(subfolder_url)
         # Make a request to the GitHub API to get the list of files in the subfolder    
         response = requests.get(subfolder_url)
         files = response.json()
+        st.write(files)
         # Find the file object with a matching name in the files list
         file_obj = next((file for file in files if file['name'] == filename), None)
         # If a matching file was found, read it and append it to the merged_dfs list
         if file_obj is not None:
+            st.write(file_obj)
             url1 = file_obj['html_url']
             raw_url = url1.replace("/blob/", "/raw/")
             df1 = pd.read_csv(raw_url)
             merged_dfs.append(df1)
 
 # Concatenate all the dataframes and display the table
-merged_df = pd.concat(merged_dfs, ignore_index=True)
-merged_df = merged_df[['justification','date']].reindex(columns=['date', 'justification'])
+    merged_df = pd.concat(merged_dfs, ignore_index=True)
+    merged_df = merged_df[['justification','date']].reindex(columns=['date', 'justification'])
 st.table(merged_df)
 
 
