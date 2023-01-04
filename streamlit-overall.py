dee26487c8147b6973d51ed37534acc13c2eed14
AmaniAli <79951845+AmaniAli95@users.noreply.github.com>
2023-01-04 13:05:51 +0800

    Update streamlit-overall.py

--- streamlit-overall.py
+++ streamlit-overall.py
@@ -5,15 +5,16 @@ import plotly.express as px
 import plotly.graph_objs as go
 import textwrap
 import requests
+import base64
+import os
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
-# Replace link_to_file with the link to the file on the cloud storage service
-link_to_file = 'https://drive.google.com/file/d/1GRvX-muzQXwC5njqugA5iQPHbUI34VYn/view?usp=share_link'
-
-# Use the requests library to download the file from the cloud storage service
-response = requests.get(link_to_file)
+url = "https://github.com/AmaniAli95/streamlit-rims/raw/main/rims.csv"
+response = requests.get(url)
 
 #df = pd.read_csv('/Users/fatinamanimohdali/Downloads/rims.csv')
-df = pd.read_csv(response.content)
+df = pd.read_csv(url)
 del df["Unnamed: 0"]
 
 st.title("Trend 2022")
@@ -58,15 +59,34 @@ st.plotly_chart(figure)
 def wrap_justification(text):
    return '<br>'.join(textwrap.wrap(text, width=60))
 
-# Create a list of dictionaries representing the data for each trace in the chart
-traces = []
+url = "https://api.github.com/repos/AmaniAli95/streamlit-rims/contents/justifi"
+response = requests.get(url)
+files = response.json()
 merged_dfs = []
-folder = '/Users/fatinamanimohdali/Documents/RIMS/14Dec2022_new/justifi/'
+
 for filename in df_totals['filename']:
-    if filename in [os.path.basename(file) for file in os.listdir(folder)]:
-        df1 = pd.read_csv(folder + filename)
+    # Find the file object with a matching name in the files list
+    file_obj = next((file for file in files if file['name'] == filename), None)
+    # If a matching file was found, read it and append it to the merged_dfs list
+    if file_obj is not None:
+        url1 = file_obj['html_url']
+        raw_url = url1.replace("/blob/", "/raw/")
+        df1 = pd.read_csv(raw_url)
         merged_dfs.append(df1)
+# Concatenate all the dataframes and display the table
     merged_df = pd.concat(merged_dfs, ignore_index=True)
     merged_df = merged_df[['justification','date']].reindex(columns=['date', 'justification'])
-    merged_df['justification'] = merged_df['justification']
 st.table(merged_df)
+
+
+
+
+#folder = '/workspaces/streamlit-rims/justifi'
+#for filename in df_totals['filename']:
+#    if filename in [os.path.basename(file) for file in files]:
+#        df1 = pd.read_csv(folder + filename)
+#        merged_dfs.append(df1)
+#    merged_df = pd.concat(merged_dfs, ignore_index=True)
+#    merged_df = merged_df[['justification','date']].reindex(columns=['date', 'justification'])
+#    merged_df['justification'] = merged_df['justification']
+#st.table(merged_df)
